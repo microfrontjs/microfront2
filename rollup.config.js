4f737e8b5677a31f657eeb02453b3873da373225
JIACHENG9 <zinkey@vip.qq.com>
2019-04-28 20:53:12 +0800

    fix: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,24 +3,22 @@ import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
-
-
 export default {
-	input: 'src/index.ts',
-	output: [{
-		file: pkg.browser,
-		format: 'umd',
-		name: 'microfront'
-	}, {
-		file: pkg.main,
-		format: 'cjs'
-	}, {
-		file: pkg.module,
-		format: 'es'
-	}],
-	plugins: [
-		typescript(),
-		resolve(),
-		commonjs()
-	],
+  input: 'src/index.ts',
+  output: [{
+    file: pkg.browser,
+    format: 'umd',
+    name: 'microfront'
+  }, {
+    file: pkg.main,
+    format: 'cjs'
+  }, {
+    file: pkg.module,
+    format: 'es'
+  }],
+  plugins: [
+    typescript(),
+    resolve(),
+    commonjs()
+  ],
 };
